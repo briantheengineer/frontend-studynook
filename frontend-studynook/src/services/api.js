bf547fbf50786c1425f60522a223d16ea62ea1fe
Brian H. Castelhano <118952129+briantheengineer@users.noreply.github.com>
2026-02-05 16:46:09 -0300

    vercel url

--- frontend-studynook/src/services/api.js
+++ frontend-studynook/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backend-chat-production-9657.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 api.interceptors.request.use(config => {
